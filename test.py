4128c37cb98a0ebc47631a2929203dbc8afe5c79
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-02-25 12:32:12 +0200

    Add useful error messages when tests fail

--- test.py
+++ test.py
@@ -12,19 +12,19 @@ class test(unittest.TestCase):
     def testDeviceWrittenToMockDatabaseRepo(self):
         mockDB = mockDatabaseRepo()
         mockDB.appendToDeviceList(deviceTest)
-        self.assertEquals(mockDB.getSizeOfListOfDevices(), 1)
+        self.assertEquals(mockDB.getSizeOfListOfDevices(), 1, "Device not added to database")
     
     def testFindDeviceBySerialNumber(self):
         mockSerial = 123
         mockDB = mockDatabaseRepo()
         mockDB.appendToDeviceList(deviceTest)
-        self.assertEquals(mockDB.getDeviceBySerial(mockSerial).returnSerialNumber(), 123)
+        self.assertEquals(mockDB.getDeviceBySerial(mockSerial).returnSerialNumber(), 123, "Device cannot be found with serial number {} ".format(mockSerial))
     
     def testFindDeviceByIMEINumber(self):
         mockIMEI = 8181
         mockDB = mockDatabaseRepo()
         mockDB.appendToDeviceList(deviceTest)
-        self.assertEquals(mockDB.getDeviceByIMEI(mockIMEI).returnIMEI(), 8181)
+        self.assertEquals(mockDB.getDeviceByIMEI(mockIMEI).returnIMEI(), 8181, "Device cannot be found with IMEI number {}".format(mockIMEI))
 
 if __name__ == '__main__':
     unittest.main()
